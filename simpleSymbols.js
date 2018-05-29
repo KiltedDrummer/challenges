c3e18ac4cc870ee1fb791d37b8bac93d92e2d44c
Nicholas Martin <nicholas.martin89@gmail.com>
2018-05-29 09:03:30 -0400

    added solution using arr.map

--- simpleSymbols.js
+++ simpleSymbols.js
@@ -28,7 +28,7 @@ SimpleSymbols(readline());
 
 
 /**
- * solved with reduce
+ * solved with arr.reduce
  */
 
 function SimpleSymbols(str) {
@@ -47,4 +47,26 @@ function SimpleSymbols(str) {
         }
         return result;
     });
+}
+
+/**
+ * solved with arr.map
+ */
+function SimpleSymbols(str) {
+
+    // code goes here
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var arr = str.split('');
+    var result;
+
+    arr.map((value, index) => {
+        if (letters.indexOf(value) > 0) {
+            if (arr[index - 1] === '+' && arr[index + 1] === '+') {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+    });
+    return result;
 }
