07cff50622110f17d47316c2dd2832c044730d55
Nicholas Martin <nicholas.martin89@gmail.com>
2018-05-29 08:57:27 -0400

    added solution with arr.reduce

---
+++ simpleSymbols.js
@@ -0,0 +1,50 @@
+/**
+ * This solved with forEach
+ * @param {*} str 
+ */
+function SimpleSymbols(str) {
+
+    // code goes here
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var arr = str.split('');
+    var result;
+
+    arr.forEach((value, index) => {
+        if (letters.indexOf(value) > 0) {
+            if (arr[index - 1] === '+' && arr[index + 1] === '+') {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+    })
+
+    return result;
+
+}
+
+// keep this function call here 
+SimpleSymbols(readline());                            
+
+
+/**
+ * solved with reduce
+ */
+
+function SimpleSymbols(str) {
+
+    // code goes here
+    var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var arr = str.split('');
+
+    return arr.reduce((result, value, index) => {
+        if (letters.indexOf(value) > 0) {
+            if (arr[index - 1] === '+' && arr[index + 1] === '+') {
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        return result;
+    });
+}
